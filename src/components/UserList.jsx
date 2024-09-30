05215044c9c787fe97e9afbe781ec361bf9aafbf
Tarun Jangra <tarunjangra043@gmail.com>
2024-09-30 12:10:27 +0530

    push

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -5,7 +5,7 @@ const UserList = () => {
   const users = [
     { id: 1, username: "Tarun Jangra" },
     { id: 2, username: "Sumit Jangra" },
-    { id: 3, username: "Tannu Jangra" },
+    { id: 3, username: "Leanne Graham" },
     { id: 4, username: "Ritik Jangra" },
     { id: 5, username: "Jonika Jangra" },
     { id: 6, username: "Deepak Jangra" },
@@ -16,13 +16,11 @@ const UserList = () => {
     <div>
       <h1>User List</h1>
       <ul>
-        {users.map((user) => {
-          return (
-            <li key={user.id}>
-              <Link to={`/users/${user.id}`}>{user.username}</Link>
-            </li>
-          );
-        })}
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={`/users/${user.id}`}>{user.username}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
