dbf9689f80926d53747f98815fda8a2214a2437b
Tarun Jangra <tarunjangra043@gmail.com>
2024-10-07 11:22:32 +0530

    Some Changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import UserList from "./components/UserList";
-import User from "./components/User";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/users/:id" element={<User />} />
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
